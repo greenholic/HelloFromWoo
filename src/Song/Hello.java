22aace4771923b61a2c2f44942f96a0e67125a01
Your Name <sy17001@byui.edu>
2019-05-18 15:55:31 -0600

    David Added changes

--- src/Song/Hello.java
+++ src/Song/Hello.java
@@ -3,6 +3,7 @@ package Song;
 public class Hello {
     public static void main(String args[]){
         System.out.println("Hello, my name is Woo");
+        System.out.println("Hello my name is DAvid and I like money.")
     }
 }
 
