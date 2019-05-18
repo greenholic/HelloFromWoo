b977be6207e1a3f3d5d4b6687398565f459f28ac
hyeonbeom lee <hyeonbeom920723@gmail.com>
2019-05-18 16:01:13 -0600

    I changed little bit more ~~

--- src/Song/Hello.java
+++ src/Song/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("Hello, my name is Woo");
         System.out.println("Hello my name is David and I like money.");
+        System.out.println("Hello my name is Hyeonbeom Lee, and I like you ~~~");
     }
 }
 
